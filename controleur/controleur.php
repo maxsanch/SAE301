acd45bd53ad55758fd8efbebaf6f1d4874de9727
Maxence_Sanchez <maxence.sanchez-varas-leclercq@uha.fr>
2024-11-22 13:48:46 +0100

    début de l'index

---
+++ controleur/controleur.php
@@ -0,0 +1,5 @@
+<?php
+
+function accueil(){
+    require "vue/vueIndex.php";
+}
--- index.php
+++ index.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ruches connectées - Accueil</title>
-    <link rel="stylesheet" media="(min-width: 620px;)" href="styles/styles_commun_ordinateur.css">
-    <link rel="stylesheet" href="styles/styles_commun_mobile.css">
-</head>
-<body>
-    
+<?php
 
-    <script src="js/commun.js"></script>
-    <script src="js/index.js"></script>
-</body>
-</html>
+//lien vers le conttrolleur
+
+
+require "controleur/controleur.php";
+
+// appel de la fonction accueil dans le controlleur qui permet d'afficher (normalement) l'index
+
+
+accueil();
