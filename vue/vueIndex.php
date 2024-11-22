e99e1b37fcd250f5800d64806dd0a1f8d54bf30a
Temecoo <goguet.noah@gmail.com>
2024-11-22 15:29:59 +0100

    Début de front end
    J'ai commencée le front end de la page d'accueil quand on est pas connecté. Actuellement y a une image, un peu de texte et des boutons mais qui marche pas encore

--- vue/vueIndex.php
+++ vue/vueIndex.php
@@ -1,15 +1,52 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ruches connectées - Accueil</title>
-    <link rel="stylesheet" media="(min-width: 620px;)" href="styles/styles_commun_ordinateur.css">
+    <link rel="stylesheet" media="(min-width: 620px)" href="styles/styles_commun_ordinateur.css">
     <link rel="stylesheet" href="styles/styles_commun_mobile.css">
 </head>
+
 <body>
-    
+    <header>
+        <div class="ConteneurHeader">
+            <div><span class="RucheHeader">R</span>uches connectées</div>
+            <div class="BoutonHeader">Se connecter</div>
+        </div>
+    </header>
+
+    <main>
+        <div class="ParentPartie1">
+            <div class="Partie1">
+                <div class="Partie1Texte">
+                    <div>Optimisez votre apiculture</div>
+                    <h1>RUCHES CONNECTEES</h1>
+                    <div class="Partie1Paragraphe">
+                        <p>Le projet des ruches connectées est un projet réalisé par un groupe d’étudiants au sein de
+                            l’IUT
+                            de Mulhouse. La formation GEII de l’IUT a réalisé un appareil capable d’effectuer
+                            différentes
+                            mesures au sein des ruches.</p>
+                        <p>Retrouvez même vos ruches perdues avec le traceur GPS ! Ce dernier suivra vos ruches et les
+                            retrouvera. Ainsi, vous ne pourrez jamais les égarer.</p>
+                    </div>
+                    <div class="Partie1Bouton">
+                        <div class="Partie1Bouton1">Découvrir plus</div>
+                        <div class="Partie1Bouton2">Se connecter</div>
+                    </div>
+                </div>
+                <div class="Partie1Image">
+                    <img src="img/RucheEtAbeille.png" alt="fdsfsd">
+                </div>
+            </div>
+        </div>
+    </main>
+
+
     <script src="js/commun.js"></script>
     <script src="js/index.js"></script>
 </body>
+
 </html>
