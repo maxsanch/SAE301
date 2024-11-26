ce0d5cb84b1cb68907c7e2dd65ba0bcbb6a6d57f
Temecoo <goguet.noah@gmail.com>
2024-11-26 15:22:37 +0100

    Continue le front
    On continue la page d'accueil quand t'es pas connecté

--- vue/vueIndex.php
+++ vue/vueIndex.php
@@ -38,8 +38,53 @@
                     </div>
                 </div>
                 <div class="Partie1Image">
-                    <img src="img/RucheEtAbeille.png" alt="fdsfsd">
+                    <img src="img/RucheEtAbeille.png" alt="Des abeille et du miel">
                 </div>
+            </div>
+        </div>
+
+        <div class="ParentPartie2">
+            <div class="Partie2">
+                <div class="Partie2Image">
+                    <img src="img/GEII.jpg" alt="Les GEII qui ont fait la ruches connectées">
+                </div>
+                <div class="Partie2Texte">
+                    <h2>Qu'est ce que le projet ruches connectées</h2>
+                    <div>
+                        <p>Réalisé par les GEII, le projet ruches connectées a d’abord été un projet de fin de semestre.
+                        </p>
+                        <p>Ce dernier avait un objectif simple : être placé à l’intérieur des ruches de l’IUT dans le
+                            but de
+                            faciliter la vie des apiculteurs. Ces derniers devaient pouvoir facilement accéder aux
+                            informations sur leurs ruches. Pour ce faire, l’appareil a permis la mesure de plusieurs
+                            valeurs.</p>
+                    </div>
+                    <div>
+                        <p>voici les mesures prises par l’appareil : </p>
+                        <ul>
+                            <li>Fréquence de battement des ailes</li>
+                            <li>température interieur de a ruche</li>
+                            <li>poids du miel</li>
+                            <li>température exterieur</li>
+                            <li>Humidité dans la ruche</li>
+                        </ul>
+                        <p>Les informations permetteront ainsi au apiculteurs de retrouver, sur ce site, les
+                            informations
+                            sur le faitqu’une ruche soit prête ou non.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="ParentPartie3">
+            <div class="Partie3">
+                <img class="Partie3Image1" src="img/Ruche.jpg" alt="Des ruches dans une clairière">
+                <div class="Partie3Image2"></div>
+                <!-- <img  class="Partie3Image2"src="img/RucheChamp.jpg" alt="Des ruches dans un champ"> -->
+                <img  class="Partie3Image3"src="img/Apiculteurs.jpg" alt="Des apiculteurs qui marchent">
+                <img  class="Partie3Image4"src="img/RucheForet.jpg" alt="Des ruches devant une foret">
+                <img  class="Partie3Image5"src="img/Abeille.jpg" alt="Des abeilles dans une ruches">
+                <img  class="Partie3Image6"src="img/Fleur.jpg" alt="Une abeille sur une fleur">
             </div>
         </div>
     </main>
