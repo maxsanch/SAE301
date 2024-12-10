5bb7452b35b92f2f27692de8e526e67a76353259
Temecoo <goguet.noah@gmail.com>
2024-12-10 14:48:35 +0100

    Normalement les pages d'accueil pour tout les rôles sont finii
    J'ai changée la couleur de fond des parties en ayant, j'ai déplacée les abeilles, j'ai changée la police des pages et j'ai ajoutée du volume aux parties en mettant du gras comme matera nous l'avait dit.

--- vue/vueIndex.php
+++ vue/vueIndex.php
@@ -34,77 +34,82 @@
                 d="M2712.62 688.616C2131.61 751.304 1624.21 426.17 1443.14 255.766C1225.5 76.0547 632.47 -185.393 1.52173 206.506"
                 stroke="#CFCFCF" stroke-width="3" stroke-dasharray="10 10" />
         </svg>
-        
+
     </div>
     <main>
-    <div class="ParentPartie1">
-        <div class="Partie1">
-            <div class="Partie1Texte">
-                <div class="SousTitre">Optimisez votre apiculture</div>
-                <h1 class="TitrePrincipal">RUCHES CONNECTEES</h1>
-                <div class="Partie1Paragraphe">
-                    <p>Le projet des ruches connectées est un projet réalisé par un groupe d’étudiants au sein de
-                        l’IUT
-                        de Mulhouse. La formation GEII de l’IUT a réalisé un appareil capable d’effectuer
-                        différentes
-                        mesures au sein des ruches.</p>
-                    <p>Retrouvez même vos ruches perdues avec le traceur GPS ! Ce dernier suivra vos ruches et les
-                        retrouvera. Ainsi, vous ne pourrez jamais les égarer.</p>
+        <div class="ParentPartie1">
+            <div class="Partie1">
+                <div class="Partie1Texte">
+                    <div class="SousTitre">Optimisez votre apiculture</div>
+                    <h1 class="TitrePrincipal">RUCHES CONNECTEES</h1>
+                    <div class="Partie1Paragraphe">
+                        <p>Le projet des ruches connectées est un projet <b>réalisé par un
+                                groupe d’étudiants</b> au sein de
+                            l’IUT
+                            de Mulhouse. La formation GEII de l’IUT a réalisé un appareil
+                            <b>capable d’effectuer
+                                différentes
+                                mesures au sein des ruches.</b>
+                        </p>
+                        <p>Retrouvez même vos ruches perdues avec le traceur GPS ! Ce dernier suivra vos ruches et les
+                            retrouvera. Ainsi, <b>vous ne pourrez jamais les égarer.</b>
+                        </p>
+                    </div>
+                    <div class="Partie1Bouton">
+                        <a href="#Information" class="Partie1Bouton1">Découvrir plus</a>
+                        <a href="index.php?page=Connexion" class="Partie1Bouton2">Se connecter</a>
+                    </div>
                 </div>
-                <div class="Partie1Bouton">
-                    <a href="#Information" class="Partie1Bouton1">Découvrir plus</a>
-                    <a href="index.php?page=Connexion" class="Partie1Bouton2">Se connecter</a>
+                <div class="Partie1Image">
+                    <img src="img/RucheEtAbeille.png" alt="Des abeille et du miel">
                 </div>
-            </div>
-            <div class="Partie1Image">
-                <img src="img/RucheEtAbeille.png" alt="Des abeille et du miel">
             </div>
         </div>
-    </div>
 
-    <div class="ParentPartie2">
-        <div class="Partie2" id="Information">
-            <div class="Partie2Image">
-                <img src="img/GEII.jpg" alt="Les GEII qui ont fait la ruches connectées">
-            </div>
-            <div class="Partie2Texte">
-                <h2 class="Partie2Titre">Qu'est ce que le projet ruches connectées</h2>
-                <div>
-                    <p>Réalisé par les GEII, le projet ruches connectées a d’abord été un projet de fin de semestre.
-                    </p>
-                    <p>Ce dernier avait un objectif simple : être placé à l’intérieur des ruches de l’IUT dans le
-                        but de
-                        faciliter la vie des apiculteurs. Ces derniers devaient pouvoir facilement accéder aux
-                        informations sur leurs ruches. Pour ce faire, l’appareil a permis la mesure de plusieurs
-                        valeurs.</p>
+        <div class="ParentPartie2">
+            <div class="Partie2" id="Information">
+                <div class="Partie2Image">
+                    <img src="img/GEII.jpg" alt="Les GEII qui ont fait la ruches connectées">
                 </div>
-                <div>
-                    <p>voici les mesures prises par l’appareil : </p>
-                    <ul>
-                        <li>Fréquence de battement des ailes</li>
-                        <li>température interieur de a ruche</li>
-                        <li>poids du miel</li>
-                        <li>température exterieur</li>
-                        <li>Humidité dans la ruche</li>
-                    </ul>
-                    <p>Les informations permetteront ainsi au apiculteurs de retrouver, sur ce site, les
-                        informations
-                        sur le faitqu’une ruche soit prête ou non.</p>
+                <div class="Partie2Texte">
+                    <h2 class="Partie2Titre">Qu'est ce que le projet ruches connectées</h2>
+                    <div>
+                        <p><b>Réalisé par les GEII</b>, le projet ruches connectées a d’abord été un projet de fin de
+                            semestre.
+                        </p>
+                        <p>Ce dernier avait un objectif simple : <b>être placé à l’intérieur des ruches de l’IUT dans le
+                                but de
+                                faciliter la vie des apiculteurs.</b> Ces derniers devaient pouvoir facilement accéder
+                            aux
+                            informations sur leurs ruches. Pour ce faire, <b>l’appareil a permis la mesure de plusieurs
+                                valeurs.</b></p>
+                    </div>
+                    <div>
+                        <p>voici les mesures prises par l’appareil : </p>
+                        <ul>
+                            <li>Fréquence de battement des ailes</li>
+                            <li>température interieur de a ruche</li>
+                            <li>poids du miel</li>
+                            <li>température exterieur</li>
+                            <li>Humidité dans la ruche</li>
+                        </ul>
+                        <p>Ce site permet aux apiculteurs de <b>savoir si le miel dans leurs ruches est prêt ou non à
+                                être récolté</b></p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="ParentPartie3">
-        <div class="Partie3">
-            <div class="Partie3Image1"></div>
-            <div class="Partie3Image2"></div>
-            <div class="Partie3Image3"></div>
-            <div class="Partie3Image4"></div>
-            <div class="Partie3Image5"></div>
-            <div class="Partie3Image6"></div>
+        <div class="ParentPartie3">
+            <div class="Partie3">
+                <div class="Partie3Image1"></div>
+                <div class="Partie3Image2"></div>
+                <div class="Partie3Image3"></div>
+                <div class="Partie3Image4"></div>
+                <div class="Partie3Image5"></div>
+                <div class="Partie3Image6"></div>
+            </div>
         </div>
-    </div>
     </main>
 
     <footer>
