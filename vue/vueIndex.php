23259211ec0d23019a426b40a7deae9baba61a28
Temecoo <goguet.noah@gmail.com>
2024-12-12 17:15:28 +0100

    Finalisation des pages d'accueil pour de vrai
    Cette fois c'est la bonne tkt mon gars. Bon par contre t'es chelou de lire ça genre franchement qui va s'embêter à lire toute la déscription, tu perds ton temps là va faire autre chose

--- vue/vueIndex.php
+++ vue/vueIndex.php
@@ -1,3 +1,7 @@
+<?php
+$footer=Footer_déconnecté
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -19,9 +23,9 @@
 
 
     <div class="Decoration">
-        <img class="AbeilleDeco1" src="../img/abeille_fond.png" alt="une petite abeille qui décore la page">
-        <img class="AbeilleDeco2" src="../img/abeille_fond.png" alt="une petite abeille qui décore la page">
-        <img class="AbeilleDeco3" src="../img/abeille_fond.png" alt="une petite abeille qui décore la page">
+        <img class="AbeilleDeco1" src="../img/abeille_fond.svg" alt="une petite abeille qui décore la page">
+        <img class="AbeilleDeco2" src="../img/abeille_fond.svg" alt="une petite abeille qui décore la page">
+        <img class="AbeilleDeco3" src="../img/abeille_fond.svg" alt="une petite abeille qui décore la page">
         <svg width="1819" height="1184" viewBox="0 0 1819 1184" fill="none" xmlns="http://www.w3.org/2000/svg"
             class="SVG1">
             <path
@@ -113,56 +117,7 @@
     </main>
 
     <footer>
-        <div class="FooterGlobal">
-            <div class="FooterPartie1">
-                <h3>RUCHES CONNECTEES</h3>
-                <div class="BordureFooterGauche">
-                    <div>Parce que permettre au apiculteurs une agriculture durable grâce à la technologie est notre
-                        devoir,
-                        le
-                        projet ruche connecté est né et permet au apiculteurs de facilement obtenir toute les
-                        informations
-                        nécéssaires facilement, avec une simle conneion sur le site !</div>
-                    <div>Facilitez vous la vie avec le projet ruches connectées.</div>
-                    <a href="#Information" class="FooterBouton1">Découvrir</a>
-                </div>
-                <div class="ParentFooterLogo">
-                    <div class="FooterLogo"><img src="img/youtube.png" alt="logo de youtube"></div>
-                    <div class="FooterLogo"><img src="img/facebook.png" alt="logo de facebook"></div>
-                    <div class="FooterLogo"><img src="img/instagram.png" alt="logo de instagram"></div>
-                </div>
-            </div>
-            <div class="FooterPartie2Parent">
-                <h4>Besoin de nous contacter ?</h4>
-                <div class="FooterPartie2">
-                    <div class="FooterPartie2SousPartie1">
-                        <div>Retrouvez nous pas mail !</div>
-                        <div>maxence.sanchez-varas-leclercq@uha.fr</div>
-                        <div>alexis.tauleigne@uha.fr</div>
-                        <div>noah.goguet@uha.fr</div>
-                        <div>gregoire.tournoux@uha.fr</div>
-                    </div>
-                    <div class="ArrangementDeLaPartie2DuFooter">
-                        <div class="FooterPartie2SousPartie2">
-                            <div>contactez nous par téléphone !</div>
-                            <div>07 83 85 16 54</div>
-                            <div>retrouvez nous !</div>
-                            <div>61 Rue Albert Camus, 68200 Mulhouse</div>
-                        </div>
-                        <div class="FooterPartie2SousPartie2">Plan du site</div>
-                    </div>
-                </div>
-                <div class="LienRapides">Liens rapides</div>
-                <div class="ListeDesActions">
-                    <a href="index.php?page=Connexion" class="Actions">Se connecter</a>
-                </div>
-            </div>
-        </div>
-        <div class="FooterPartieBasseBordure"></div>
-        <div class="FooterPartieBasse">
-            <div>Projet ruches connectées</div>
-            <div>Mentions légales</div>
-        </div>
+        <?= $footer ?>
     </footer>
 
 
--- vue/vueIndexConnecteAdmin.php
+++ vue/vueIndexConnecteAdmin.php
@@ -1,3 +1,7 @@
+<?php
+$footer = Footer_connecté;
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -29,9 +33,9 @@
     </header>
 
     <div class="Decoration">
-        <img class="AbeilleDeco1" src="../img/abeille_fond.png" alt="une petite abeille qui décore la page">
-        <img class="AbeilleDeco2" src="../img/abeille_fond.png" alt="une petite abeille qui décore la page">
-        <img class="AbeilleDeco3" src="../img/abeille_fond.png" alt="une petite abeille qui décore la page">
+        <img class="AbeilleDeco1" src="../img/abeille_fond.svg" alt="une petite abeille qui décore la page">
+        <img class="AbeilleDeco2" src="../img/abeille_fond.svg" alt="une petite abeille qui décore la page">
+        <img class="AbeilleDeco3" src="../img/abeille_fond.svg" alt="une petite abeille qui décore la page">
         <svg width="1819" height="1184" viewBox="0 0 1819 1184" fill="none" xmlns="http://www.w3.org/2000/svg"
             class="SVG1">
             <path
@@ -208,59 +212,7 @@
     </main>
 
     <footer>
-        <div class="FooterGlobal">
-            <div class="FooterPartie1">
-                <h3>RUCHES CONNECTEES</h3>
-                <div class="BordureFooterGauche">
-                    <div>Parce que permettre au apiculteurs une agriculture durable grâce à la technologie est notre
-                        devoir,
-                        le
-                        projet ruche connecté est né et permet au apiculteurs de facilement obtenir toute les
-                        informations
-                        nécéssaires facilement, avec une simle conneion sur le site !</div>
-                    <div>Facilitez vous la vie avec le projet ruches connectées.</div>
-                    <a href="#Information" class="FooterBouton1">Découvrir ></a>
-                </div>
-                <div class="ParentFooterLogo">
-                    <div class="FooterLogo"><img src="../img/youtube.png" alt="logo de youtube"></div>
-                    <div class="FooterLogo"><img src="../img/facebook.png" alt="logo de facebook"></div>
-                    <div class="FooterLogo"><img src="../img/instagram.png" alt="logo de instagram"></div>
-                </div>
-            </div>
-            <div class="FooterPartie2Parent">
-                <h4>Besoin de nous contacter ?</h4>
-                <div class="FooterPartie2">
-                    <div class="FooterPartie2SousPartie1">
-                        <div>Retrouvez nous pas mail !</div>
-                        <div>maxence.sanchez-varas-leclercq@uha.fr</div>
-                        <div>alexis.tauleigne@uha.fr</div>
-                        <div>noah.goguet@uha.fr</div>
-                        <div>gregoire.tournoux@uha.fr</div>
-                    </div>
-                    <div class="ArrangementDeLaPartie2DuFooter">
-                        <div class="FooterPartie2SousPartie2">
-                            <div>contactez nous par téléphone !</div>
-                            <div>07 83 85 16 54</div>
-                            <div>retrouvez nous !</div>
-                            <div>61 Rue Albert Camus, 68200 Mulhouse</div>
-                        </div>
-                        <div class="FooterPartie2SousPartie2">Plan du site</div>
-                    </div>
-                </div>
-                <div class="LienRapides">Lien rapides</div>
-                <div class="ListeDesActions">
-                    <a href="index.php?page=Notes" class="Actions">Mes notes</a>
-                    <a href="index.php?page=Gestion" class="Actions">Gestion des ruches</a>
-                    <a href="index.php?page=Ruches" class="Actions">Suivi des ruches</a>
-                    <a href="index.php?action=quitter" class="Actions">Se déconnecter</a>
-                </div>
-            </div>
-        </div>
-        <div class="FooterPartieBasseBordure"></div>
-        <div class="FooterPartieBasse">
-            <div>Projet ruches connectées</div>
-            <div>Mentions légales</div>
-        </div>
+        <?= $footer ?>
     </footer>
 
     <script src="../js/commun.js"></script>
